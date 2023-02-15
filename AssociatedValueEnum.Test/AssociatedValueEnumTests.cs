ec3f03eaa490bf3133f1512b5b216c24d4dc5f49
Denis Murphy <denismurp@gmail.com>
2023-02-15 10:24:29 +0000

    Code Example

---
+++ AssociatedValueEnum.Test/AssociatedValueEnumTests.cs
@@ -0,0 +1,38 @@
+﻿namespace AssociatedValueEnum.Tests
+{
+    public class BarcodeTests
+    {
+        [Fact]
+        public void TestUPCBarcode()
+        {
+            IBarcode barcode = new UPC { NumberSystem = 1, Manufacturer = 2, Product = 3, Check = 4 };
+            switch (barcode)
+            {
+                case UPC upc:
+                    Assert.Equal(1, upc.NumberSystem);
+                    Assert.Equal(2, upc.Manufacturer);
+                    Assert.Equal(3, upc.Product);
+                    Assert.Equal(4, upc.Check);
+                    break;
+                case QRCode qrCode:
+                    Assert.True(false, "Unexpected QRCode barcode type");
+                    break;
+            }
+        }
+
+        [Fact]
+        public void TestQRCodeBarcode()
+        {
+            IBarcode barcode = new QRCode { ProductCode = "Hello World" };
+            switch (barcode)
+            {
+                case UPC upc:
+                    Assert.True(false, "Unexpected UPC barcode type");
+                    break;
+                case QRCode qrCode:
+                    Assert.Equal("Hello World", qrCode.ProductCode);
+                    break;
+            }
+        }
+    }
+}
---
+++ AssociatedValueEnum/AssociatedValueEnum.cs
@@ -0,0 +1,30 @@
+﻿namespace AssociatedValueEnum
+{
+    public interface IBarcode {
+
+        public UPC Upc(int numberSystem, int manufacturer, int product, int check)
+        {
+            return new UPC { NumberSystem = numberSystem, Manufacturer = manufacturer, Product = product, Check = check };
+        }
+
+        public QRCode QrCode(string productCode)
+        {
+            return new QRCode { ProductCode = productCode };
+        }
+
+    }
+
+    public struct UPC : IBarcode
+    {
+        public int NumberSystem { get; set; }
+        public int Manufacturer { get; set; }
+        public int Product { get; set; }
+        public int Check { get; set; }
+    }
+
+    public struct QRCode : IBarcode
+    {
+        public string ProductCode { get; set; }
+    }  
+
+}
